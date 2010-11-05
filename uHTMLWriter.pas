cc81ad59a44eb05b55bebd5d89d48e062750bf0a
Nick <Nick@localhost>
2010-11-05 15:55:46 -0400

    another change for the sake of hudson.

--- uHTMLWriter.pas
+++ uHTMLWriter.pas
@@ -360,7 +360,7 @@ type
 
   { TODO -oNick : Add <frame>  support even though frames are the spawn of satan. Seriously.   }
 
-  { TODO -oNick : add <map> <area> support so people can build image maps.   }
+  { TODO -oNick : add <map> <area> support so people can build image maps. Which are cool.  }
 
   { TODO -oNick : Add <object> <param> support.  Need to make complete list of missing tags. }
 
