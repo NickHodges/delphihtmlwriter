1dc359827d8259f13855995f80c9c10b7cbfdb09
Nick <Nick@localhost>
2011-01-08 19:08:25 -0500

    Moving /// documentation over to the HTMLWriterIntf.pas unit

--- HTMLWriterIntf.pas
+++ HTMLWriterIntf.pas
@@ -19,12 +19,16 @@ type
 
 
     function AddTag(aString: string; aCloseTagType: TCloseTagType = ctNormal; aCanAddAttributes: TCanHaveAttributes = chaCanHaveAttributes): IHTMLWriter;
+    /// <summary>Opens a&lt;head&gt; tag to the document.  </summary>
     function OpenHead: IHTMLWriter;
     function OpenMeta: IHTMLWriter;
     function OpenBase: IHTMLWriter;
     function OpenBaseFont: IHTMLWriter;
+    ///	<summary>Adds a &lt;base /&gt; tag to the HTML.</summary>
+    ///	<remarks>Note: This method can only be called inside an open &lt;head&gt; tag.</remarks>
     function AddBase(aTarget: TTargetType; aFrameName: string = ''): IHTMLWriter; overload;
     function AddBase(aHREF: string): IHTMLWriter; overload;
+    /// <summary>Opens a &lt;title&gt; tag.</summary>
     function OpenTitle: IHTMLWriter;
     {$REGION 'Documentation'}
     /// <summary>Adds a &lt;title&gt; tag including the passed in text.</summary>
@@ -33,6 +37,7 @@ type
     {$ENDREGION}
     function AddTitle(aTitleText: string): IHTMLWriter;
     function AddMetaNamedContent(aName: string; aContent: string): IHTMLWriter;
+    /// <summary>Opens a &lt;body&gt; tag.</summary>
     function OpenBody: IHTMLWriter;
     function OpenParagraph: IHTMLWriter;
     function OpenParagraphWithStyle(aStyle: string): IHTMLWriter;
@@ -160,14 +165,29 @@ type
     function OpenFieldSet: IHTMLWriter;
     function OpenLegend: IHTMLWriter;
     function AddLegend(aText: string): IHTMLWriter;
+    /// <summary>Opens an &lt;iframe&gt; tag.</summary>
     function OpenIFrame: IHTMLWriter; overload;
+    /// <summary>Opens an &lt;iframe&gt; tag and adds a url parameter</summary>
+    /// <param name="aURL">The value to be added with the url parameter.</param>
     function OpenIFrame(aURL: string): IHTMLWriter; overload;
     function OpenIFrame(aURL: string; aWidth: THTMLWidth; aHeight: integer): IHTMLWriter; overload;
     function AddIFrame(aURL: string; aAlternateText: string): IHTMLWriter; overload;
     function AddIFrame(aURL: string; aAlternateText: string; aWidth: THTMLWidth; aHeight: integer): IHTMLWriter; overload;
+    /// <summary>Opens an unordered list tag (&lt;ul&gt;)</summary>
+    /// <param name="aBulletShape">An optional parameter indicating the bullet type that the list should use.</param>
+    /// <seealso cref="TBulletShape">TBulletShape</seealso>
     function OpenUnorderedList(aBulletShape: TBulletShape = bsNone): IHTMLWriter;
+    {$REGION 'Documentation'}
+    /// <summary>Opens an ordered list tag (&lt;ol&gt;)</summary>
+    /// <param name="aNumberType">An optional parameter indicating the numbering type that the list should use.</param>
+    /// <seealso cref="TNumberType">TNumberType</seealso>
+    {$ENDREGION}
+
     function OpenOrderedList(aNumberType: TNumberType = ntNone): IHTMLWriter;
+    /// <summary>Opens a list item (&lt;li&gt;) inside of a list.</summary>
     function OpenListItem: IHTMLWriter;
+    /// <summary>Adds a List item (&lt;li&gt;) with the given text</summary>
+    /// <param name="aText">The text to be added to the list item.</param>
     function AddListItem(aText: string): IHTMLWriter;
     procedure LoadFromFile(const FileName: string); overload;
     procedure LoadFromFile(const FileName: string; Encoding: TEncoding); overload;
@@ -177,14 +197,36 @@ type
     procedure SaveToFile(const FileName: string; Encoding: TEncoding); overload;
     procedure SaveToStream(Stream: TStream); overload;
     procedure SaveToStream(Stream: TStream; Encoding: TEncoding); overload;
+    ///	<summary>Opens a &lt;frameset&gt; tag.</summary>
+    ///	<remarks>This tag is not part of the HTML5 specification.</remarks>
     function OpenFrameset: IHTMLWriter;
+    {$REGION 'Documentation'}
+    /// <summary>Opens a &lt;frame&gt; tag.</summary>
+    /// <exception cref="ENotInFrameSetHTMLException">Raised if this is called outside of a &lt;frameset&gt;
+    /// tag.</exception>
+    ///	<remarks>This tag is not part of the HTML5 specification.</remarks>
+    {$ENDREGION}
     function OpenFrame: IHTMLWriter;
+    /// <summary>Opens a &lt;noframes&gt; tag.</summary>
+    ///	<remarks>This tag is not part of the HTML5 specification.</remarks>
     function OpenNoFrames: IHTMLWriter;
+    /// <summary>Opens a &lt;map /&gt; tag</summary>
     function OpenMap: IHTMLWriter;
+    /// <summary>Opens an &lt;area /&gt; tag</summary>
     function OpenArea: IHTMLWriter;
+    /// <summary>Opens an &lt;object&gt; tag</summary>
     function OpenObject: IHTMLWriter;
+    {$REGION 'Documentation'}
+    /// <summary>Opens a &lt;param&gt; tag</summary>
+    /// <param name="aName">The name for the parameter</param>
+    /// <param name="aValue">The value to be assigned to the paramter</param>
+    /// <remarks>This tag can only be used inside of an &lt;object&gt; tag.</remarks>
+    /// <exception cref="ENotInObjectTagException">Raised if this method is called outside of an &lt;object&gt;
+    /// tag</exception>
+    {$ENDREGION}
     function OpenParam(aName: string; aValue: string = ''): IHTMLWriter; // name parameter is required
     property Attribute[const Name: string; const Value: string]: IHTMLWriter read GetAttribute; default;
+    ///	<summary>Property determining the level of error reporting that the class should provide.</summary>
     property ErrorLevels: THTMLErrorLevels read GetErrorLevels write SetErrorLevels;
     property HTML: TStringBuilder read GetHTML;
 
