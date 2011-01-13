b4d104c16eb3a06e761970c5b784cde3a38df43d
Nick Hodges <nickhodges@gmail.com>
2011-01-13 09:36:56 -0500

    closing test branch

---
+++ Test/HTMLWriterTestAppTests.dpr
@@ -0,0 +1,42 @@
+program HTMLWriterTestAppTests;
+
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  TestuHTMLWriter in 'TestuHTMLWriter.pas',
+  uHTMLWriter in '..\uHTMLWriter.pas',
+  HTMLWriterUtils in '..\HTMLWriterUtils.pas',
+  HTMLWriterIntf in '..\HTMLWriterIntf.pas',
+  LoadSaveIntf in '..\LoadSaveIntf.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+  {$IFDEF USEXML}
+    XMLTestRunner.RunRegisteredTests('HTMLWriterTestAppTests.xml').Free
+  {$ELSE}
+    TextTestRunner.RunRegisteredTests(rxbHaltOnFailures).Free
+  {$ENDIF}
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
