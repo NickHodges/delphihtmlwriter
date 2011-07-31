1f20c37a9c942762501869a740acacfca48c5f64
Nick Hodges <nickhodges@gmail.com>
2011-07-31 18:21:04 -0400

    Declared consts for the IHTML5 tags

--- HTMLWriterUtils.pas
+++ HTMLWriterUtils.pas
@@ -406,6 +406,38 @@ type
       cCloseComment = '-->';
       cSpace = ' ';
       cEmptyString = '';
+
+      {IHTML5 Constants}
+
+      cArticle     = 'Article';
+      cAside       = 'Aside';
+      cAudio       = 'Audio';
+      cCanvas      = 'Canvas';
+      cCommand     = 'Command';
+      cDatalist    = 'Datalist';
+      cDetails     = 'Details';
+      cEmbed       = 'Embed';
+      cFigCaption  = 'FigCaption';
+      cFigure      = 'Figure';
+      cFooter      = 'Footer';
+      cHeader      = 'Header';
+      cHGroup      = 'HGroup';
+      cKeyGen      = 'KeyGen';
+      cMark        = 'Mark';
+      cMeter       = 'Meter';
+      cNav         = 'Nav';
+      cOutput      = 'Output';
+      cProgress    = 'Progress';
+      cRP          = 'RP';
+      cRT          = 'RT';
+      cRuby        = 'Ruby';
+      cSection     = 'Section';
+      cSource5     = 'Source';
+      cSummary     = 'Summary';
+      cTime        = 'Time';
+      cVideo       = 'Video';
+      cWBR         = 'WBR';
+
    {$ENDREGION}
 
     THTMLDocTypeStrings: array [THTMLDocType] of string = ('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">', '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">', '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">');
